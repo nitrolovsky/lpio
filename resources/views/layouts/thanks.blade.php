72d5806cee1d260ed8ff7d350a5ac72801c3381f
Pavel L <nitrolovsky@gmail.com>
2017-05-26 11:52:53 +0300

    Fixed metrika

--- resources/views/layouts/thanks.blade.php
+++ resources/views/layouts/thanks.blade.php
@@ -12,13 +12,14 @@
             {{ Request::server('PATH_INFO') }}
         </title>
         <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-            ga('create', 'UA-97200678-1', 'auto');
-            ga('send', 'pageview');
+          ga('create', 'UA-99764557-1', 'auto');
+          ga('send', 'pageview');
+
         </script>
     </head>
     <body>
@@ -31,14 +32,13 @@
             (function (d, w, c) {
                 (w[c] = w[c] || []).push(function() {
                     try {
-                        w.yaCounter44126189 = new Ya.Metrika({
-                            id:44126189,
+                        w.yaCounter44736544 = new Ya.Metrika({
+                            id:44736544,
                             clickmap:true,
                             trackLinks:true,
                             accurateTrackBounce:true,
                             webvisor:true,
-                            trackHash:true,
-                            ecommerce:"dataLayer"
+                            trackHash:true
                         });
                     } catch(e) { }
                 });
@@ -55,7 +55,7 @@
                 } else { f(); }
             })(document, window, "yandex_metrika_callbacks");
         </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/44126189" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <noscript><div><img src="https://mc.yandex.ru/watch/44736544" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
         <!-- /Yandex.Metrika counter -->
         <script src="/js/main.js"></script>
     </body>
