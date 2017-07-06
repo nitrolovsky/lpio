ea28691b9063b4f3ea7b4feb91f95de74ba8d07f
Pavel L <nitrolovsky@gmail.com>
2017-07-06 09:22:18 +0300

    Updated magic-tracks

---
+++ resources/views/components/slide.blade.php
@@ -0,0 +1,15 @@
+<div class="container-fluid py-5 font">
+    <div class="row">
+        <div class="col-xl-6 col-lg-6 col-md-12 col-12">
+            <img src="{{ $image }}" class="img-fluid mx-auto d-block sw">
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-12 col-12">
+            <h4 class="black w-5">
+                {{ $title }}
+            </h4>
+            <div class="s-17">
+                {{ $text }}
+            </div>
+        </div>
+    </div>
+</div>
