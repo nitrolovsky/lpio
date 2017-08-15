42794e4fd73179263f547caa20b0f5058333bf53
Pavel L <nitrolovsky@gmail.com>
2017-08-15 15:30:11 +0300

    Updated ehlektronika

--- resources/views/pages/ehlektronika.blade.php
+++ resources/views/pages/ehlektronika.blade.php
@@ -72,40 +72,36 @@
     @endslot
 
     @slot('leadmagnet')
-        Получите прайс-лист и расчет вашего заказа
+        Оставьте заявку
     @endslot
 
     @slot('form')
-        <form action="/leads" method="POST">
-            {{ csrf_field() }}
-            <input type="hidden" name="source" value="ehlektronika">
-            <input type="hidden" name="cta" value="Форма с видео">
+        <form id="__vtigerWebForm" name="wdelab " action="http://aemlab.ru/work/run/wdelab/modules/Webforms/capture.php" method="post" accept-charset="utf-8" enctype="multipart/form-data"><input type="hidden" name="__vtrftk" value="sid:f3d7c43eef146aff3ff16ac49988f2452c1dda47,1501662433">
+            <input type="hidden" name="publicid" value="7a10efb385c7727a6a4d2a4d307c3351">
+            <input type="hidden" name="urlencodeenable" value="1">
+            <input type="hidden" name="name" value="wdelab ">
             <div class="form-group">
-                <input type="text" class="font black form-control w-4" id="name" placeholder="Имя Фамилия" name="name">
+                <input type="text" class="font black form-control w-4" id="name" placeholder="Имя*" name="firstname" data-label="" value="" required="">
             </div>
             <div class="form-group">
-                <input type="text" class="font black form-control w-4" id="email" placeholder="Email" name="email">
+                <input type="text" class="font black form-control w-4" id="name" placeholder="Фамилия*" name="lastname" data-label="" value="" required="">
             </div>
             <div class="form-group">
-                <input type="text" class="font black form-control w-4" id="phone" placeholder="Телефон" name="phone" required="required">
+                <input type="email" class="font black form-control w-4" id="name" placeholder="Адрес электронной почты*" name="email" data-label="" value="" required="">
             </div>
             <div class="form-group">
-                <textarea class="form-control font black w-4" id="comment" rows="3" name="comment" placeholder="Комментарий"></textarea>
+                <input type="email" class="font black form-control w-4" id="name" placeholder="Телефон*" name="phone" data-label="" value="" required="">
+            </div>
+            <div class="form-group">
+                <textarea class="form-control font black w-4" id="comment" rows="3" name="description" placeholder="Описание*" required=""></textarea>
             </div>
             <div class="form-group">
                 <button type="submit" class="font btn btn-primary btn-block w-4" role="button">
-                    Получить
+                    Отправить
                 </button>
             </div>
-            <div class="form-group mb-0">
-                <div class="form-check mb-0">
-                    <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" checked>
-                    </label>
-                    Согласен на обработку персональных данных и <a href="../files/beton/politic.pdf" class="text-primary a" target="_blank">политикой конфиденциальности</a>
-                </div>
-            </div>
         </form>
+        <script  type="text/javascript">window.onload = function() { var N=navigator.appName, ua=navigator.userAgent, tem;var M=ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);if(M && (tem= ua.match(/version\/([\.\d]+)/i))!= null) M[2]= tem[1];M=M? [M[1], M[2]]: [N, navigator.appVersion, "-?"];var browserName = M[0];var form = document.getElementById("__vtigerWebForm"), inputs = form.elements; form.onsubmit = function() { var required = [], att, val; for (var i = 0; i < inputs.length; i++) { att = inputs[i].getAttribute("required"); val = inputs[i].value; type = inputs[i].type; if(type == "email") {if(val != "") {var elemLabel = inputs[i].getAttribute("label");var emailFilter = /^[_/a-zA-Z0-9]+([!"#$%&()*+,./:;<=>?\^_`{|}~-]?[a-zA-Z0-9/_/-])*@[a-zA-Z0-9]+([\_\-\.]?[a-zA-Z0-9]+)*\.([\-\_]?[a-zA-Z0-9])+(\.?[a-zA-Z0-9]+)?$/;var illegalChars= /[\(\)\<\>\,\;\:\"\[\]]/ ;if (!emailFilter.test(val)) {alert("For "+ elemLabel +" field please enter valid email address"); return false;} else if (val.match(illegalChars)) {alert(elemLabel +" field contains illegal characters");return false;}}}if (att != null) { if (val.replace(/^\s+|\s+$/g, "") == "") { required.push(inputs[i].getAttribute("label")); } } } if (required.length > 0) { alert("The following fields are required: " + required.join()); return false; } var numberTypeInputs = document.querySelectorAll("input[type=number]");for (var i = 0; i < numberTypeInputs.length; i++) { val = numberTypeInputs[i].value;var elemLabel = numberTypeInputs[i].getAttribute("label");var elemDataType = numberTypeInputs[i].getAttribute("datatype");if(val != "") {if(elemDataType == "double") {var numRegex = /^[+-]?\d+(\.\d+)?$/;}else{var numRegex = /^[+-]?\d+$/;}if (!numRegex.test(val)) {alert("For "+ elemLabel +" field please enter valid number"); return false;}}}var dateTypeInputs = document.querySelectorAll("input[type=date]");for (var i = 0; i < dateTypeInputs.length; i++) {dateVal = dateTypeInputs[i].value;var elemLabel = dateTypeInputs[i].getAttribute("label");if(dateVal != "") {var dateRegex = /^[1-9][0-9]{3}-(0[1-9]|1[0-2]|[1-9]{1})-(0[1-9]|[1-2][0-9]|3[0-1]|[1-9]{1})$/;if(!dateRegex.test(dateVal)) {alert("For "+ elemLabel +" field please enter valid date in required format"); return false;}}}}; }</script>
     @endslot
 
     @slot('legal')
