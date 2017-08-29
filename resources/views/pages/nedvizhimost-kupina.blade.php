2ebdf22dd49f10c5e679a33bf1ba16dab33c7f12
Pavel L <nitrolovsky@gmail.com>
2017-08-29 17:17:00 +0300

    Updated gtm

--- resources/views/pages/nedvizhimost-kupina.blade.php
+++ resources/views/pages/nedvizhimost-kupina.blade.php
@@ -3,6 +3,23 @@
         Ремонт холодольников СПб
     @endslot
 
+    @slot('gtm_head')
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-56WTNTG');</script>
+        <!-- End Google Tag Manager -->
+    @endslot
+
+    @slot('gtm_body')
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-56WTNTG"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+    @endslot
+    
     @slot('google_analytics')
         <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -29,7 +46,7 @@
         })(window, document, 'script', 'cloud.roistat.com', '7fb8a6edcf4799069d4b8ee6b97487fa');
         </script>
     @endslot
-    
+
     @slot('bg_video')
         '../files/remont-holodilnikov-doma/remont-holodilnikov-doma.mp4'
     @endslot
