133700eb8d138e00281f4afe299e1aa96c5c5a4f
nitrolovsky <nitrolovsky@gmail.com>
2017-10-16 16:50:24 +0300

    Removed speech

--- config/app.php
+++ config/app.php
@@ -177,8 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Pyatnitsev\YandexSpeech\YandexSpeechServiceProvider::class,
-
     ],
 
     /*
