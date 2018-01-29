5be9b8dc930f4ce073f4b8af912402887470d1a8
nitrolovsky <nitrolovsky@gmail.com>
2018-01-29 17:58:05 +0300

    Updated lpio

--- resources/views/pages/rospis-graffiti-ru-1.blade.php
+++ resources/views/pages/rospis-graffiti-ru-1.blade.php
@@ -1,4 +1,4 @@
-@component('components.onescreen')
+@component('components.bullet')
     @slot('title')
         Художественная роспись стен, фреска, граффити
     @endslot
@@ -16,15 +16,11 @@
     @endslot
 
     @slot('lptracker')
-        <script async type="text/javascript">function loadscript(e,t){var n=document.createElement("script");n.src="//lptracker.net.ru/"+e;n.onreadystatechange=t;n.onload=t;document.head.appendChild(n);return 1}var init_lstats=function(){lstats.site_id=42192;lstats.referer()};var jquery_lstats=function(){jQstat.noConflict();loadscript("stats_auto.js",init_lstats)};loadscript("jquery-1.10.2.min.js",jquery_lstats);</script>
+
     @endslot
 
-    @slot('bg_video')
-        '../files/rospis-graffiti-ru/rospis-graffiti-ru.mp4'
-    @endslot
-
-    @slot('bg_poster')
-        '../files/rospis-graffiti-ru/rospis-graffiti-ru.jpg'
+    @slot('video')
+        'wW3tplmhGTE'
     @endslot
 
     @slot('brand')
@@ -36,69 +32,60 @@
     @endslot
 
     @slot('phone')
-        88124269298
+        +78124269298
     @endslot
 
     @slot('phone_view')
-        8 (812) 426 92 98
+        +7 (812) 426 92 98
     @endslot
 
     @slot('email')
         magicmagicart@gmail.com
     @endslot
 
-    @slot('address')
-
-    @endslot
-
     @slot('offer')
-        Художественная роспись стен интерьеров от 1500р/м2. Cначала рисуем. Оплата после выполненной работы
+        Роспись стен интерьеров и фасадов от 750 р./м2. Cначала рисуем.<br>Оплата после выполненной работы
     @endslot
 
     @slot('bullets')
         <li>
-            Граффити на заказ, роспись кистями
+            Граффити на заказ, роспись стен кистями
         </li>
         <li class="pt-1">
-            Аэрография, нанесение фрески
+            Аэрография стен, нанесение фрески
         </li>
         <li class="pt-1">
-            15 художников с образованием и чувством стиля
+            Портфолио 200+ проектов
         </li>
         <li class="pt-1">
-            200 работ в портфолио, делаем эксклюзив
+            Защищаем рисунки лаком, стены можно мыть
         </li>
         <li class="pt-1">
-            От 5 лет, опыт каждого художника
+            13+ лет опыта работы у художников
         </li>
         <li class="pt-1">
             2000+ м2 теперь радуют взгляд заказчиков
         </li>
+        <li class="pt-1">
+            Выполняем срочные заказы в срок
+        </li>
     @endslot
 
-    @slot('video')
-        https://www.youtube.com/embed/5PsoGwqhMjs
-    @endslot
-
-    @slot('lead_magnet')
-        Получите крутейшее портфолио, заполнив форму
+    @slot('leadmagnet')
+        Получите расчет стоимости росписи в 3 вариантах + их визуализацию бесплатно, заполнив форму
     @endslot
 
     @slot('form')
         <form action="/leads" method="POST">
             {{ csrf_field() }}
-            <input type="hidden" name="source" value="all_leads">
-            <input type="hidden" name="to" value="magicmagicart@gmail.com">
+            <input type="hidden" name="source" value="beton">
             <input type="hidden" name="cta" value="Форма с видео">
             <div class="form-group">
-                <input type="text" class="font black form-control w-4" id="email" placeholder="Введите email" name="email">
-            </div>
-            <div class="form-group">
-                <input type="text" class="font black form-control w-4" id="phone" placeholder="Телефон" name="phone" required="required">
+                <input type="text" class="font black form-control w-4" id="phone" placeholder="Введите телефон" name="phone" required="required">
             </div>
             <div class="form-group">
                 <button type="submit" class="font btn btn-primary btn-block w-4" role="button">
-                    Получить
+                    Получить расчет проекта
                 </button>
             </div>
             <div class="text-center">
