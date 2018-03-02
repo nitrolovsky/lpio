f40a37b400d03c84af1966a59384ccca0b539b4b
nitrolovsky <nitrolovsky@gmail.com>
2018-03-02 10:59:51 +0300

    updated roistat

--- resources/views/components/lp20180129.blade.php
+++ resources/views/components/lp20180129.blade.php
@@ -90,6 +90,15 @@
         <noindex><script async src="https://stats.lptracker.ru/code/new/50584"></script></noindex>
     </head>
     <body>
+        <script>
+        (function(w, d, s, h, id) {
+            w.roistatProjectId = id; w.roistatHost = h;
+            var p = d.location.protocol == "https:" ? "https://" : "http://";
+            var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
+            var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+        })(window, document, 'script', 'cloud.roistat.com', '9137bf8d093afa7b0efef9e9f67378ed');
+        </script>
+
         {{ $yandex or '' }}
 
         {{ $navbar or '' }}
